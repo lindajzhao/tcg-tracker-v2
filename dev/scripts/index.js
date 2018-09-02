8a7912aef872c8f76a0405a15a75745629870f2c
Natasha Teng <nteng3290@gmail.com>
2018-09-02 17:44:59 -0400

    Importing Redux

--- dev/scripts/index.js
+++ dev/scripts/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import Router from './components/Router';
 import { FIREBASE_CONFIG } from './config';
+import Store from './store';
 
 // Initialize Firebase
 class Index extends React.Component {
@@ -19,6 +20,15 @@ class Index extends React.Component {
 
     // TODO-REDUX: Draw from redux `store`
     this.firebase.auth().onAuthStateChanged((user) => {
+      // Push to redux store
+      const action = {
+        type : "test_event",
+        payload: {
+          loggedIn: !!(user),
+          user,
+        }
+      }
+      Store.dispatch(action);
       this.setState({
         loggedIn: !!(user),
         user,
