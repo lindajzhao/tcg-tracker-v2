f40bf9804cb3f8eac3f7753c560bb2ab915f8412
Natasha Teng <nteng3290@gmail.com>
2018-09-08 17:59:02 -0400

    Removing comments

--- dev/scripts/components/CardGridPage.js
+++ dev/scripts/components/CardGridPage.js
@@ -35,16 +35,6 @@ class CardGridPage extends React.Component {
 
   componentDidMount() {
     this.setState({ page: 1 });
-    // 1. get cards into state somehow
-    // if there are cards in this set in redux(filtering going on here)
-    // this.setState({ reduxCards })
-
-    // if there aren't cards in redux already
-    // make axios call
-    // add results to redux
-
-    // recheck(?)
-    console.log(Store.getState(), "ComponentDidMount");
     if (Store.getState().allCardsInSet.length){
       console.log("Cards are in Redux", Store.getState());
     }
@@ -54,7 +44,6 @@ class CardGridPage extends React.Component {
   }
 
   loadCards(page, set) {
-    console.log("Loading Cards")
     // create the parameters and headers for axios call
 
     // make axio calls to retrive all cards in set
@@ -66,7 +55,6 @@ class CardGridPage extends React.Component {
         pageSize: '20',
       },
     }).then((res) => {
-      console.log("Cards Loaded");
       const allCards = {
         type: "set_cards",
         payload:{
@@ -79,7 +67,6 @@ class CardGridPage extends React.Component {
         loadedCards: true,
       });
       Store.dispatch(allCards);
-      console.log("storing cards to redux");
     });
   }
 
@@ -119,13 +106,13 @@ class CardGridPage extends React.Component {
   }
 
   searchBySet(e) {
-    // this.setState({
-    //   allCardsInSet: [],
-    //   showFilteredCards: false,
-    //   filteredCards: [],
-    //   page: 1,
-    //   set: e.target.value,
-    // });
+    this.setState({
+      allCardsInSet: [],
+      showFilteredCards: false,
+      filteredCards: [],
+      page: 1,
+      set: e.target.value,
+    });
     this.loadCards(this.state.page, e.target.value);
   }
 
